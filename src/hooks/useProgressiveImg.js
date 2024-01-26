40b5596428fcb56ea57c439df3b75154c141afc9
Michael Lisboa <michael.lisboa@gmail.com>
2024-01-26 15:33:02 +0100

    updated images from Dinda

--- src/hooks/useProgressiveImg.js
+++ src/hooks/useProgressiveImg.js
@@ -1,26 +1,20 @@
 import { useState, useEffect } from 'react';
 
 const useProgressiveImage = (highResSrc) => {
-  const deriveLowResSrc = (src) => {
-    if (typeof src === 'string') {
-      return src.replace(/([^/]+)(?=\.\w+$)/, 'tiny-$1');
-    } else if (src && src.default) {
-      const path = src.default;
-      return path.replace(/([^/]+)(?=\.\w+$)/, 'tiny-$1');
-    }
-    return src;
-  };
+  const deriveLowResSrc = (src) => 
+    typeof src === 'string' || (src && src.default)
+      ? (src.default || src).replace(/([^/]+)(?=\.\w+$)/, 'tiny-$1')
+      : src;
 
   const lowResSrc = deriveLowResSrc(highResSrc);
   const [src, setSrc] = useState(lowResSrc);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const highResImagePath = (typeof highResSrc === 'object' && highResSrc.default) ? highResSrc.default : highResSrc;
     const img = new Image();
-    img.src = highResImagePath;
+    img.src = typeof highResSrc === 'object' && highResSrc.default ? highResSrc.default : highResSrc;
     img.onload = () => {
-      setSrc(highResImagePath);
+      setSrc(img.src);
       setLoading(false);
     };
   }, [highResSrc]);
@@ -29,3 +23,4 @@ const useProgressiveImage = (highResSrc) => {
 };
 
 export default useProgressiveImage;
+
