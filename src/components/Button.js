803fee88c13e532c6309a92d0c074a71ba8e39dd
Michael Lisboa <michael.lisboa@gmail.com>
2024-01-27 17:03:13 +0100

    update to button component to reduce vertical height

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,27 +2,63 @@ import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
-import buttonDefaultImage from '../images/button_default.png';
+import buttonDefaultImage from '../images/button_default@2x.png';
 import buttonHoverImage from '../images/button_hover.png';
 import buttonActiveImage from '../images/button_active.png';
 
 
+// const StyledLink = styled(Link)`
+//   position: relative;
+//   z-index: 2;
+//   display: inline-flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+//   padding: 0;
+//   height: 72px;
+//   min-width: 276px;
+//   border: solid 0px transparent;
+//   border-width: 40px 32px;
+//   border-image: url(${buttonDefaultImage}) 0 0 repeat;
+//   font-family: "Inknut Antiqua", Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+//   font-weight: 800;
+//   font-size: 16px;
+//   color: #322F20;
+//   cursor: pointer;
+//   outline: none;
+//   box-sizing: border-box;
+//   background-color: transparent;
+//   white-space: nowrap;
+//   transition: border-image 0.35s ease-in-out, min-width 0.35s ease-in-out;
+
+//   border-image: ${props => props.isactive ? `url(${buttonActiveImage}) 48 40 stretch !important` : `url(${buttonDefaultImage}) 48 40 repeat !important`};
+
+//   &:hover {
+//     text-decoration: none;
+//     color: initial;
+//     border-image: url(${buttonHoverImage}) 48 40 stretch;
+//   }
+// `;
+
 const StyledLink = styled(Link)`
-  position: relative;
+position: relative;
   z-index: 2;
   display: inline-flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 0 24px;
-  height: 96px;
-  min-width: 276px;
+  padding: 0;
+  height: 72px;
+  min-width: 248px;
   border: solid 0px transparent;
-  border-width: 48px 40px;
-  border-image: url(${buttonDefaultImage}) 48 40 repeat;
+border-width: 20px 28px;
+  // -moz-border-image: url(${buttonDefaultImage}) 32 80 fill repeat round;
+  // -webkit-border-image: url(${buttonDefaultImage}) 32 80 fill repeat round;
+  // -o-border-image: url(${buttonDefaultImage}) 32 80 fill repeat round;
+  border-image: url(${buttonDefaultImage}) 44 64 fill stretch stretch;
   font-family: "Inknut Antiqua", Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
   font-weight: 800;
-  font-size: 18px;
+  font-size: 16px;
   color: #322F20;
   cursor: pointer;
   outline: none;
@@ -31,13 +67,14 @@ const StyledLink = styled(Link)`
   white-space: nowrap;
   transition: border-image 0.35s ease-in-out, min-width 0.35s ease-in-out;
 
-  border-image: ${props => props.isactive ? `url(${buttonActiveImage}) 48 40 repeat !important` : `url(${buttonDefaultImage}) 48 40 repeat !important`};
+  border-image: ${props => props.isactive ? `url(${buttonActiveImage}) 44 64 fill stretch stretch; !important` : `url(${buttonDefaultImage}) 44 64 fill stretch stretch; !important`};
 
   &:hover {
     text-decoration: none;
     color: initial;
-    border-image: url(${buttonHoverImage}) 48 40 repeat;
+    border-image: url(${buttonHoverImage}) 44 64 fill stretch stretch;
   }
+
 `;
 
 const NextButton = motion(StyledLink);
