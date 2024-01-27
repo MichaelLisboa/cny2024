6ce161cc957fa7762439a0ad56d83165cebff292
Michael Lisboa <michael.lisboa@gmail.com>
2024-01-27 18:03:46 +0100

    a bunch of experiments to smooth the animation for fucking safari

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,47 +1,13 @@
 import React, { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import buttonDefaultImage from '../images/button_default@2x.png';
 import buttonHoverImage from '../images/button_hover.png';
 import buttonActiveImage from '../images/button_active.png';
 
-
-// const StyledLink = styled(Link)`
-//   position: relative;
-//   z-index: 2;
-//   display: inline-flex;
-//   flex-direction: column;
-//   align-items: center;
-//   justify-content: center;
-//   padding: 0;
-//   height: 72px;
-//   min-width: 276px;
-//   border: solid 0px transparent;
-//   border-width: 40px 32px;
-//   border-image: url(${buttonDefaultImage}) 0 0 repeat;
-//   font-family: "Inknut Antiqua", Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
-//   font-weight: 800;
-//   font-size: 16px;
-//   color: #322F20;
-//   cursor: pointer;
-//   outline: none;
-//   box-sizing: border-box;
-//   background-color: transparent;
-//   white-space: nowrap;
-//   transition: border-image 0.35s ease-in-out, min-width 0.35s ease-in-out;
-
-//   border-image: ${props => props.isactive ? `url(${buttonActiveImage}) 48 40 stretch !important` : `url(${buttonDefaultImage}) 48 40 repeat !important`};
-
-//   &:hover {
-//     text-decoration: none;
-//     color: initial;
-//     border-image: url(${buttonHoverImage}) 48 40 stretch;
-//   }
-// `;
-
 const StyledLink = styled(Link)`
-position: relative;
+  position: relative;
   z-index: 2;
   display: inline-flex;
   flex-direction: column;
@@ -51,28 +17,34 @@ position: relative;
   height: 72px;
   min-width: 248px;
   border: solid 0px transparent;
-border-width: 20px 28px;
-  // -moz-border-image: url(${buttonDefaultImage}) 32 80 fill repeat round;
-  // -webkit-border-image: url(${buttonDefaultImage}) 32 80 fill repeat round;
-  // -o-border-image: url(${buttonDefaultImage}) 32 80 fill repeat round;
+  border-width: 20px 28px;
+  -moz-border-image: url(${buttonDefaultImage}) 44 64 fill stretch stretch;
+  -webkit-border-image: url(${buttonDefaultImage}) 44 64 fill stretch stretch;
+  -o-border-image: url(${buttonDefaultImage}) 44 64 fill stretch stretch;
   border-image: url(${buttonDefaultImage}) 44 64 fill stretch stretch;
   font-family: "Inknut Antiqua", Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
   font-weight: 800;
-  font-size: 16px;
+  font-size: 1rem;
   color: #322F20;
   cursor: pointer;
   outline: none;
   box-sizing: border-box;
   background-color: transparent;
   white-space: nowrap;
-  transition: border-image 0.35s ease-in-out, min-width 0.35s ease-in-out;
-
-  border-image: ${props => props.isactive ? `url(${buttonActiveImage}) 44 64 fill stretch stretch; !important` : `url(${buttonDefaultImage}) 44 64 fill stretch stretch; !important`};
+  transform: translate3d(0, 0, 0); /* Force hardware acceleration */
+  transition: transform 0.35s ease-in-out, min-width 0.35s ease-in-out, font-size 0.35s ease-in-out;
+  will-change: transform, min-width, font-size;
 
   &:hover {
     text-decoration: none;
     color: initial;
+    font-size: 1.25rem;
     border-image: url(${buttonHoverImage}) 44 64 fill stretch stretch;
+    -moz-border-image: url(${buttonHoverImage}) 44 64 fill stretch stretch;
+    -webkit-border-image: url(${buttonHoverImage}) 44 64 fill stretch stretch;
+    -o-border-image: url(${buttonHoverImage}) 44 64 fill stretch stretch;
+    transform: translate3d(0, -4px, 0);
+    will-change: border-image, min-width, font-size;
   }
 
 `;
