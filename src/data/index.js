299bdd5eb93261ff2dff265a3ee5acd043832090
Michael Lisboa <michael.lisboa@gmail.com>
2024-02-07 13:15:38 +0100

    updated copy from kathleen and more progress on calligraphy

--- src/data/index.js
+++ src/data/index.js
@@ -4,7 +4,7 @@ export const elementsList = [
         title: "Water",
         text: "Misty lakeside with soothing ripples",
         subheadline: "You've risen from",
-        description: "As you embrace the Water element, a serene energy envelops you. Ripples of adaptability and resilience flow through your essence. Now, let the currents guide your journey."
+        description: "As you embrace the Water element, a serene energy envelops you. You let the currents guide you on a journey of adaptability and resilience!"
     },
     {
         image: require('../images/elements/wood.png'),
@@ -18,19 +18,19 @@ export const elementsList = [
         title: "Fire",
         text: "Blaze-lit cavern glowing with warmth",
         subheadline: "You've risen from",
-        description: "It blazes within you, igniting passion and determination. Flames of courage flicker, ready to illuminate the path ahead. Embrace the warmth and stride with determination."
+        description: "It blazes within you, igniting passion and determination. Flames of courage flicker, ready to illuminate the path ahead. Embrace the warmth and stride with determination!"
     },
     {
         image: require('../images/elements/earth.png'),
         title: "Earth",
         text: "Cliffside overlooking the horizon",
         subheadline: "You've risen from",
-        description: "The sturdy foundation beneath your feet, anchors your spirit. With stability and responsibility, you embark on a journey grounded in purpose. Feel the solidity of the earth as you step into the unknown."
+        description: "The sturdy foundation beneath your feet anchors your spirit. With stability and responsibility, you embark on a journey grounded in purpose. Feel the soil beneath your feet as you step into the unknown."
     },
     {
         image: require('../images/elements/metal.png'),
         title: "Metal",
-        text: "Shimmering scape under moonlight",
+        text: "Shimmering scape under the moonlight",
         subheadline: "You've risen from",
         description: "It shapes your being with discipline and precision. Your spirit, akin to a well-forged blade, is ready to cut through challenges. Embrace the strength of metal as you carve your destiny."
     },
@@ -47,21 +47,21 @@ export const traitsList = [
     {
         title: "Clever Minded",
         image: require('../images/traits/cleverness.png'),
-        description: "The elixir of cleverness enhances your wit and intelligence. You navigate the *element_noun* with strategic acumen, outsmarting spirits and solving challenges with ease. Your sharp mind becomes your greatest asset, and other spirits admire your clever approach.",
+        description: "The elixir of cleverness enhances your wit and intelligence. You navigate the *element_noun* with a strategic mind, outsmarting spirits and solving challenges with ease. Your sharp mind becomes your greatest asset, and other spirits admire your clever approach.",
         subheadline: "Drink from the cup of Cleverness",
         text: ""
     },
     {
         title: "Energetic Spirit",
         image: require('../images/traits/energy.png'),
-        description: "The vibrant energy from the cup infuses your spirit with agility and speed. You dart through the *element_noun*, embracing the thrill of the race. Your energetic spirit sets a lively pace, making you a formidable contender among the spirits.",
+        description: "Infused with vibrant energy from the cup, your spirit gains agility and speed, allowing you to dart through the *element_noun*, and embrace the thrill of the race. Your lively spirit makes you a competitive contender among others.",
         subheadline: "Drink from the cup of Energy",
         text: ""
     },
     {
         title: "Crafty Nature",
         image: require('../images/traits/crafty.png'),
-        description: "The elixir of craftiness imbues you with ingenuity and sly tactics. Navigating through the *element_noun*, you employ clever tricks and strategies, gaining an edge over challenges. Your crafty nature becomes a hallmark of your journey.",
+        description: "Imbued with the elixir of craftiness, you gain ingenuity and sly tactics, enabling you to navigate through the *element_noun* with clever tricks and strategies, gaining an edge over challenges. Your crafty nature becomes a hallmark of your journey.",
         subheadline: "Drink from the cup of Craftiness",
         text: ""
     },
@@ -82,14 +82,14 @@ export const traitsList = [
     {
         title: "Persevering Spirit",
         image: require('../images/traits/persevering.png'),
-        description: "The elixir of perseverance fortifies your spirit with unwavering determination. Among the ancient *element_noun*, obstacles may attempt to hinder your path, but you press on, fueled by an inner strength that resonates with the very heartbeat of the *element_noun*.",
-        subheadline: "Drink from the cup of Perseverence",
+        description: "The elixir of perseverance strengthens your spirit with determination. Among the ancient *element_noun*, obstacles may attempt to hinder your path, but you press on, fueled by an inner strength that resonates with the very heartbeat of the *element_noun*.",
+        subheadline: "Drink from the cup of Perseverance",
         text: ""
     },
     {
-        title: "Walks with Grace",
+        title: "Walk with Grace",
         image: require('../images/traits/graceful.png'),
-        description: "The cup of grace bestows upon you an aura of elegance and finesse. Moving through the enchanted *element_noun*, your every step is marked by graceful precision. Challenges in the way seem to bow before your graceful spirit.",
+        description: "Bestowed with an aura of elegance and finesse from the cup of grace, you move through the enchanted *element_noun*, each step marked by graceful precision, as challenges along the way seem to bow before your graceful spirit.",
         subheadline: "Drink from the cup of Gracefulness",
         text: ""
     },
@@ -103,7 +103,7 @@ export const traitsList = [
     {
         title: "Curious Soul",
         image: require('../images/traits/curiosity.png'),
-        description: "The elixir of curiosity sparks a deep desire to explore. Every challenge, every encounter becomes an opportunity to learn. Your curious spirit leads you down uncharted paths, uncovering hidden wonders in the *element_noun*.",
+        description: "The elixir of curiosity sparks a deep desire to explore, turning every challenge and encounter into an opportunity to learn, leading your curious spirit down uncharted paths and uncovering hidden wonders in *element_noun*.",
         subheadline: "Drink from the cup of Curiosity",
         text: ""
     },
@@ -113,65 +113,65 @@ export const allianceList = [
     {
         title: "Clever Rat",
         image: require('../images/zodiac_actual/animals/rat.png'),
-        description: "I'm known for my wit. Would you join forces with me to navigate these challenges?",
-        reaction: "Rat scuttles excitedly, saying,",
+        description: "I am known for my wit. Will you join forces with me to navigate these challenges?",
+        reaction: "Rat scurries excitedly, saying,",
         text: "Ah, great choice, spirit! Let's outsmart the competition!"
     },
     {
         title: "Strong Ox",
         image: require('../images/zodiac_actual/animals/ox.png'),
-        description: "My strength is unmatched. Will you join forces to overcome these challenges?",
+        description: "My strength is unmatched. Shall we join forces to overcome these challenges?",
         reaction: "With sturdy trot, Ox declares,",
         text: "A worthy adversary! Brace yourself, spirit, for a test of strength."
     },
     {
         title: "Fierce Tiger",
         image: require('../images/zodiac_actual/animals/tiger.png'),
-        description: "To win this race, you'll need courage and strength. Will you join me to learn the ways of the *element_noun*?",
-        reaction: "Tiger prowls confidently, remarking,",
-        text: "Fearless! We'll tackle obstacles like the kings of the jungle."
+        description: "To win this race, you will need courage and strength. Join me to learn the ways of the *element_noun*",
+        reaction: "Tiger stands with a commanding presence, declaring,",
+        text: "We shall navigate the trials ahead, not as followers, but as sovereigns of our path."
     },
     {
         title: "Smart Rabbit",
         image: require('../images/zodiac_actual/animals/rabbit.png'),
-        description: "I'm known for my quick thinking. Would you like to team up and outpace the competition?",
-        reaction: "Rabbit hops gracefully, stating,",
-        text: "Smart move, spirit! Quick thinking will be our ticket to victory."
+        description: "I am known for my agility and wit. Would you like to team up and outpace the competition?",
+        reaction: "Rabbit hops lightly, stating,",
+        text: "Smart move, spirit! Quick thinking will be our path to victory."
     },
     {
         title: "Wise Dragon",
         image: require('../images/zodiac_actual/animals/dragon.png'),
-        description: "I dare you to navigate this treacherous obstacle course. Let's see if you can succeed.",
-        reaction: "Dragon glides with majestic movements, chuckles,",
-        text: "Bravery, I like! Let's see if you can soar through challenges, spirit."
+        description: "Only the brave dare to navigate this treacherous course. Let's see if you can succeed.",
+        reaction: "With a sweep of its powerful wings, the Dragon hovers,",
+        text: "Bravery, I like! Let us navigate the twists of fate and rise above the challenges, spirit."
     },
     {
         title: "Patient Snake",
         image: require('../images/zodiac_actual/animals/snake.png'),
-        description: "I sense intricate challenges on our path. With patience, we can overcome them together. Would you join me?",
+        description: "Ahead lies a path woven with complexity. With patience, we can overcome them together. Shall we navigate it as allies?",
         reaction: "Snake slithers with finesse, hissing,",
-        text: "A brave choice, spirit! Let's slither through these challenges together."
+        text: "Wisssely chosen, spirit! With cunning and patience, we'll navigate the twisssts of fate together."
     },
     {
         title: "Mighty Horse",
         image: require('../images/zodiac_actual/animals/horse.png'),
-        description: "I've been watching your progress. It's time for a test of strength. Are you up for the challenge?",
-        reaction: "Horse gallops energetically, neighing,",
-        text: "Fantastic! Our strength will carry us to success. repare for a true test of your spirit's might!"
+        description: "It is time for a test of strength. Are you up for the challenge?",
+        reaction: "Horse gallops forward with vibrant energy,",
+        text: "Our strength will carry us to success. Prepare for a true test of your spirit's might!"
     },
     {
         title: "Polite Goat",
         image: require('../images/zodiac_actual/animals/goat.png'),
-        description: "I see you've come a long way in this race. With cooperation, we can navigate them gracefully.",
-        reaction: "Goat trots amiably, bleating,",
-        text: "A brave soul! Let's approach challenges with grace."
+        description: "Your journey thus far is admirable. Together, with our combined resilience and strategic thinking, we can overcome the hurdles with ease. Shall we join forces?",
+        reaction: "Goat nods in greeting,",
+        text: "Welcome, kindred spirit! With wisdom and cooperation, we'll scale heights and overcome any obstacle."
     },
     {
         title: "Curious Monkey",
         image: require('../images/zodiac_actual/animals/monkey.png'),
-        description: "I've heard tales of mysterious artifacts hidden in the heart of the *element_noun*. What do you say?",
-        reaction: "Monkey swings playfully, chattering,",
-        text: "Curiosity is our guide! Let's explore and triumph."
+        description: "Rumors of hidden treasures within the heart of *element_noun* have reached my ears. Shall we embark on this quest together?",
+        reaction: "Monkey leaps excitedly, eyes sparkling with anticipation",
+        text: "Curiosity is key! Let's explore and unveil our victory!"
     },
     {
         title: "Determined Rooster",
@@ -181,18 +181,18 @@ export const allianceList = [
         text: "Bravo! With determination, we'll conquer all obstacles."
     },
     {
-        title: "Amiable Dog",
+        title: "Friendly Dog",
         image: require('../images/zodiac_actual/animals/dog.png'),
-        description: "With amiability and camaraderie, we can make the journey enjoyable. Would you be my companion?",
+        description: "With a loyal friend by your side, every step of the journey becomes a delightful adventure. Would you be my companion?",
         reaction: "Dog bounds joyfully, barking,",
-        text: "Loyal choice! We'll face challenges with endurance."
+        text: "Oh, splendid choice! With you as my companion, there's nothing we can't conquer!"
     },
     {
-        title: "Energetic Pig",
+        title: "Chill Pig",
         image: require('../images/zodiac_actual/animals/pig.png'),
-        description: "I challenge you to a race through the forest. Are you energetic enough to keep up?",
-        reaction: "Pig trots contentedly, oinking,",
-        text: "Delightful! Together, we'll navigate with ease."
+        description: "Fancy a leisurely stroll through the forest together? I bet we could make a great team!",
+        reaction: "Pig rolls over, oinking,",
+        text: "Delightful! I'm more of a stroll-at-my-own-pace kind of pig, but we could still have a good time together."
     },
 ];
 
@@ -299,13 +299,13 @@ export const pathsList = [
     {
         title: "Ancient Shrine Route",
         image: require('../images/paths/ancient-shrine-route.png'),
-        description: "As you venture deeper on the Ancient Shrine route. The path is dimly lit by the soft glow of  lanterns, casting flickering shadows on the shrine's walls. Clink! You stumble upon something on the ground.",
+        description: "As you venture deeper on the Ancient Shrine route. The path is dimly lit by the soft glow of lanterns, casting flickering shadows on the shrine's walls. Clink! You stumble upon something on the ground.",
         resultText: "you two explored the Ancient Shrine Route"
     },
     {
         title: "Cascade Waterfall Path",
         image: require('../images/paths/cascade-waterfall-path.png'),
-        description: "Beside the thundering waterfall, the *alliance_noun* awaits, its powerful presence matching the force of the cascading water. But suddenly there's a clinking sound. You stumble upon something on the ground.",
+        description: "Beside the thundering waterfall, the *alliance_noun* awaits, its powerful presence matching the force of the cascading water. Suddenly there's a clinking sound. You stumble upon something on the ground.",
         resultText: "you two explored the Ancient Shrine Route"
     },
     {
@@ -326,6 +326,39 @@ export const puzzleData = [
         successMessage: "With a graceful sweep, you and the *alliance_noun* gather the broken pieces and arrange them in a harmonious pattern. In the journey ahead, remember the strength that lies within restoration.",
         failTitle: "In the fragments of failure, seeds of wisdom are sown.",
         failMessage: "The porcelain remains fragmented, scattered across the ground. It seems time slipped away faster than your efforts to restore it. The *alliance_noun* urges you to learn from this experience and carry the lessons of impermanence and the value of time into the unfolding journey.",
+        puzzlesList: [
+            {
+                title: "Crafted Porcelain Puzzle",
+                image: require('../images/jigsaw/pottery-finished-1.jpg'),
+                description: "The porcelain pieces lie shattered, scattered across the ground. As you piece together the fragments, a sense of accomplishment fills your spirit.",
+                subheadline: "Swap the tiles to restore.",
+            },
+            {
+                title: "Mysterious Artifact Puzzle",
+                image: require('../images/jigsaw/pottery-finished-2.jpg'),
+                description: "A mysterious artifact lies in pieces, waiting to be restored. As you piece together the fragments, a sense of accomplishment fills your spirit.",
+                subheadline: "Swap the tiles to restore.",
+            },
+            {
+                title: "Mystical Relic Puzzle",
+                image: require('../images/jigsaw/pottery-finished-3.jpg'),
+                description: "A mystical relic lies in pieces, waiting to be restored. As you piece together the fragments, a sense of accomplishment fills your spirit.",
+                subheadline: "Swap the tiles to restore.",
+            },
+        ]
+    }
+];
+
+export const calligraphyData = [
+    {
+        headline: "The mist slowly shifts, guiding your gaze to a sacred scroll adorned with Chinese calligraphy characters.",
+        subheadline: "Your wit tells you that you need to rewrite the calligraphy in the scroll presented in front of you. Will you face the challenge?",
+        challengeHeadline: "Rewrite the characters. Consider the curves, the angles, the spaces between.",
+        challengeMessage: "Choose the applicable strokes.",
+        successTitle: "Calligraphy Master",
+        successMessage: "You have harnessed the art of calligraphy, a reflection of your mastery over the written expression of your true self. Embrace this newfound skill as you continue your journey.",
+        failTitle: "Failure is not an end but a lesson indeed.",
+        failMessage: "The characters on the stone tablet do not align in harmony. The celestial realm is patient, embrace the opportunity to refine your understanding.",
         puzzlesList: [
             {
                 title: "Crafted Porcelain Puzzle",
--- src/pages/test-your-calligraphy-skills.js
+++ src/pages/test-your-calligraphy-skills.js
@@ -1,31 +1,166 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React, { useContext, useState, useEffect, useMemo } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import styled from 'styled-components';
+import { motion, useAnimation } from 'framer-motion';
+import { AppContext } from '../contexts/AppContext';
+import { useDynamicTextReplacer } from '../hooks/useDynamicTextReplacer';
 import pages from '../utils/pages';
 import Layout from '../templates/layout';
-import { AppContext } from '../contexts/AppContext';
+import Image from '../components/Image';
+import { OrnateButton, OptionButton } from '../components/Button';
+import { calligraphyData } from '../data';
 import CalligraphyGame from '../components/react-mal-calligraphy';
+import scroll from "../images/calligraphy/scroll.png";
+import success from "../images/tokens/puzzle.png";
+import fail from "../images/tokens/failed_puzzle.png";
+
+const SplashImage = styled(Image)`
+    img {
+        width: 100%;
+        object-fit: contain;
+    }
+`;
+
+const ButtonContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    padding-left: 16px !important;
+    padding-right: 16px !important;
+
+    * {
+        font-size: 1rem;
+
+        @media (max-width: 576px) { // When the viewport is 576px or less
+            font-size: 0.875rem !important; // Reduce the font size even more
+        }
+    }
+`;
+
+const HeaderSection = styled(motion.div)`
+  // Add your header-section styles here.
+`;
+
+const BodySection = styled(motion.div)`
+  // Add your body-section styles here.
+  justify-content: flex-start;
+  text-align: center !important;
+  padding-top: 0;
+
+  .body-section-wide {
+    align-self: center;
+    width: 100% !important;
+   }
+`;
+
+const FooterSection = styled(motion.div)`
+  // Add your footer-section styles here.
+`;
 
 const TestYourCalligraphySkills = () => {
-    const { userState, updateUserInfo } = useContext(AppContext) || JSON.parse(localStorage.getItem('userState'));
-    const location = useLocation();
-    const currentPage = pages.find(page => page.url === location.pathname);
-    const nextPage = pages.find(page => page.url === currentPage.nextPage);
-    const previousPage = pages.find(page => page.url === currentPage.previousPage);
+    const { updateUserSelection, getUserInfo } = useContext(AppContext);
+    const replaceElementNoun = useDynamicTextReplacer();
+    const [content, setContent] = useState('initial');
+    const [isGameComplete, setIsGameComplete] = useState(null);
     const [refreshEnabled, setRefreshEnabled] = useState(true);
+    const location = useLocation();
+    const navigate = useNavigate();
+    const currentPage = useMemo(() => pages.find(page => page.url === location.pathname), [location.pathname]);
+    const nextPage = useMemo(() => pages.find(page => page.url === currentPage.nextPage), [currentPage]);
+    const previousPage = useMemo(() => pages.find(page => page.url === currentPage.previousPage), [currentPage]);
+    const headerControls = useAnimation();
+    const bodyControls = useAnimation();
+    const footerControls = useAnimation();
+
+    const characters = calligraphyData[0].puzzlesList;
+    const [randomCharacter, setRandomCharacter] = useState(characters[Math.floor(Math.random() * characters.length)]);
+
 
     useEffect(() => {
-        // Disable refresh on mount
         setRefreshEnabled(false);
-    
-        // Re-enable refresh when the component unmounts
         return () => setRefreshEnabled(true);
-      }, [setRefreshEnabled]);
+    }, [setRefreshEnabled]);
+
+    const animateExit = async () => {
+        await footerControls.start({ y: 100, opacity: 0 });
+        await bodyControls.start({ y: 100, opacity: 0 });
+        await headerControls.start({ y: 100, opacity: 0 });
+    };
+
+    const animateEnter = async () => {
+        await headerControls.start({ y: 0, opacity: 1, transition: { delay: 0.01 } });
+        await bodyControls.start({ y: 0, opacity: 1, transition: { delay: 0.025 } });
+        await footerControls.start({ y: 0, opacity: 1, transition: { delay: 0.05 } });
+    };
+
+    const handleButtonClick = async () => {
+        await animateExit();
+        setContent('game');
+        await animateEnter();
+    };
 
     return (
-            <Layout refreshEnabled={refreshEnabled}>
-                <CalligraphyGame />
-                <Link to={`${nextPage.url}`}>Go to {nextPage.title}</Link>
-            </Layout>
+        <Layout refreshEnabled={refreshEnabled}>
+            <HeaderSection
+                animate={headerControls}
+                className="header-section"
+            >
+                {content === 'initial' ? (
+                    <div className="mal-margin-bottom-small mal-padding-remove-horizontal">
+                        <h3 className="mal-margin-remove-vertical">{replaceElementNoun(calligraphyData[0].headline)}</h3>
+                        <p className="mal-text-medium mal-margin-small-top">{replaceElementNoun(calligraphyData[0].subheadline)}</p>
+                    </div>
+                ) : content === 'game' ? null
+                    : (
+                        <div className="mal-padding-remove-horizontal">
+                            <h3 className="mal-margin-remove-vertical">{replaceElementNoun(randomCharacter.title)}</h3>
+                            <p className="mal-text-medium mal-margin-small-top mal-margin-small-bottom">{replaceElementNoun(randomCharacter.description)}</p>
+                            <p className="mal-text-small mal-margin-remove-vertical padding-remove-vertical">Swap the tiles to restore the {replaceElementNoun(randomCharacter.title.replace(/\s*Puzzle\s*/g, ""))}.</p>
+                        </div>
+                    )}
+            </HeaderSection>
+            <BodySection
+                animate={bodyControls}
+                className="body-section"
+            >
+                {content === 'initial' ? (
+                    <div className="body-section-wide">
+                        <SplashImage src={scroll} alt="Calligraphy scroll" />
+                    </div>
+                ) : content === 'game' ? (
+                    // Display game related content here
+                    <CalligraphyGame />
+                ) : content === 'complete' ? (
+                    'Stuff'
+                ) : null}
+
+            </BodySection>
+            <FooterSection
+                animate={footerControls}
+                className="footer-section">
+                {content === 'initial' ? (
+                    <ButtonContainer>
+                        <OptionButton onClick={
+                            async () => {
+                                updateUserSelection('calligraphyChallengeResult', false);
+                                await animateExit();
+                                navigate(nextPage.url);
+                            }
+                        }>
+                            No, let's move on
+                        </OptionButton>
+                        <OptionButton onClick={handleButtonClick}>
+                            Yes, let's do it
+                        </OptionButton>
+                    </ButtonContainer>
+                ) : content === 'game' ? (
+                    // Display game related content here
+                    'placeholder'
+                ) : content === 'complete' ? (
+                    <OptionButton to={`${nextPage.url}`}>Go to {nextPage.title}</OptionButton>
+                ) : null}
+            </FooterSection>
+        </Layout>
     );
 };
 
