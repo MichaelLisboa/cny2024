2e1d85885c08333cfec66c7be26c4784e562325e
Michael Lisboa <michael.lisboa@gmail.com>
2024-02-08 23:40:39 +0100

    updates to calligraphy

--- src/games/CalligraphyFlashGame.js
+++ src/games/CalligraphyFlashGame.js
@@ -3,6 +3,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 import styled from 'styled-components';
 import Image from '../components/Image';
 import Timer from '../components/Timer';
+import { OrnateButton, OptionButton } from '../components/Button';
 
 import graceful from '../images/calligraphy/graceful.svg';
 import clever from '../images/calligraphy/clever.svg';
@@ -19,43 +20,40 @@ import brave from '../images/calligraphy/brave.svg';
 // Styled Components
 const GameContainer = styled.div`
   text-align: center;
-  padding: 20px;
+  // padding: 20px;
 `;
 
 const FlashCardContainer = styled(motion.div)`
   margin: 20px auto;
 `;
 
-const NextButton = styled.button`
+const CharacterButton = styled.button`
   padding: 10px 20px;
-  margin-top: 20px;
   cursor: pointer;
+  background-color: transparent;
+  border: none;
+`;
+
+const ButtonContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    padding-left: 16px !important;
+    padding-right: 16px !important;
+
+    * {
+        font-size: 1rem;
+
+        @media (max-width: 576px) { // When the viewport is 576px or less
+            font-size: 0.875rem !important; // Reduce the font size even more
+        }
+    }
 `;
 
 const OptionsGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  gap: 10px;
-  margin-top: 20px;
-`;
-
-const OptionButton = styled.button`
-  padding: 10px;
-  cursor: pointer;
-  background-color: transparent;
-  border: 2px solid #ccc;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  &:hover {
-    background-color: #f0f0f0;
-  }
-
-  img {
-    width: 100%;
-    max-width: 60px;
-  }
+  gap: 24px;
 `;
 
 // Animation Variants
@@ -81,7 +79,6 @@ const characterList = [
 ];
 
 const shuffleArray = (array) => array.sort(() => Math.random() - 0.5);
-const getRandomIndex = (length) => Math.floor(Math.random() * length);
 
 const CalligraphyFlashGame = ({ timeLimit = 60 }) => {
   const [activeCharacterIndex, setActiveCharacterIndex] = useState(0);
@@ -89,7 +86,6 @@ const CalligraphyFlashGame = ({ timeLimit = 60 }) => {
   const [isTimeUp, setIsTimeUp] = useState(false);
   const [timerStarted, setTimerStarted] = useState(false);
 
-  // Preparing character options with useMemo
   const characterOptions = useMemo(() => {
     let options = shuffleArray(characterList.filter((_, index) => index !== activeCharacterIndex)).slice(0, 8);
     options.push(characterList[activeCharacterIndex]);
@@ -117,15 +113,17 @@ const CalligraphyFlashGame = ({ timeLimit = 60 }) => {
           <FlashCardContainer key="activeCharacter" variants={variants} initial="hidden" animate="visible" exit="exit">
             <Image src={characterList[activeCharacterIndex].image} alt={characterList[activeCharacterIndex].english} />
           </FlashCardContainer>
-          <NextButton onClick={handleButtonClick}>{timerStarted ? "Next" : "Begin"}</NextButton>
+          <ButtonContainer>
+            <OptionButton onClick={handleButtonClick}>{timerStarted ? "Next" : "Begin"}</OptionButton>
+          </ButtonContainer>
         </>
       ) : (
         <AnimatePresence>
           <OptionsGrid key="characterOptions" variants={variants} initial="hidden" animate="visible" exit="exit">
             {characterOptions.map((option, index) => (
-              <OptionButton key={index} onClick={() => handleCharacterChoice(option)}>
-                <img src={option.image} alt={option.english} />
-              </OptionButton>
+              <CharacterButton key={index} onClick={() => handleCharacterChoice(option)}>
+                <Image src={option.image} alt={option.english} />
+              </CharacterButton>
             ))}
           </OptionsGrid>
         </AnimatePresence>
