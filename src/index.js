226bdf000fc4108afaa96d581c3de693c4d31733
Michael Lisboa <michael.lisboa@gmail.com>
2024-02-09 14:25:26 +0100

    added react-ga

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga';
 import './styles/global.css';
+
+ReactGA.initialize('G-4SZM7TT0QN');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
