c2b1cec6316be0a37de26aee4d8d6df11db44ce8
Michael Lisboa <michael.lisboa@gmail.com>
2024-02-11 15:01:44 +0100

    Always Title case _nouns

--- src/hooks/useDynamicTextReplacer.js
+++ src/hooks/useDynamicTextReplacer.js
@@ -17,13 +17,15 @@ export const useDynamicTextReplacer = () => {
   };
 
   const replaceText = (text) => {
+    const titleCase = (str) => str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+
     const replacements = {
-      '\\*element_noun\\*': userInfo?.chosenElement.choice || getDataFromLocalStorage('chosenElement').choice || '',
+      '\\*element_noun\\*': titleCase(userInfo?.chosenElement.choice || getDataFromLocalStorage('chosenElement').choice || ''),
       '\\*element_endResult\\*': userInfo?.chosenElement.element_endResult || getDataFromLocalStorage('chosenElement').element_endResult || '',
-      '\\*alliance_noun\\*': userInfo?.chosenAlliance.choice || getDataFromLocalStorage('chosenAlliance').choice || '',
+      '\\*alliance_noun\\*': titleCase(userInfo?.chosenAlliance.choice || getDataFromLocalStorage('chosenAlliance').choice || ''),
       '\\*alliance_endResult\\*': userInfo?.chosenAlliance.alliance_endResult || getDataFromLocalStorage('chosenAlliance').alliance_endResult || '',
-      '\\*trait_noun\\*': userInfo?.chosenTrait || getDataFromLocalStorage('chosenTrait') || '',
-      '\\*path_noun\\*': userInfo?.chosenPath.choice || getDataFromLocalStorage('chosenPath').choice || '',
+      '\\*trait_noun\\*': titleCase(userInfo?.chosenTrait || getDataFromLocalStorage('chosenTrait') || ''),
+      '\\*path_noun\\*': titleCase(userInfo?.chosenPath.choice || getDataFromLocalStorage('chosenPath').choice || ''),
       '\\*path_endResult\\*': userInfo?.chosenPath.path_endResult || getDataFromLocalStorage('chosenPath').path_endResult || '',
       '\\*riddle_endResult\\*': userInfo?.riddleResult.riddle_endResult || getDataFromLocalStorage('riddleResult').riddle_endResult || '',
       '\\*puzzle_endResult\\*': userInfo?.potteryPuzzleResult.puzzle_endResult || getDataFromLocalStorage('potteryPuzzleResult').puzzle_endResult || '',
